294e19002f60f9a1c7b4e504b96732bd8d1a7456
ASUS-A455l <nuraniiin38@gmail.com>
2016-11-13 21:12:09 +0700

    3

--- app/src/main/java/id/sch/smktelkom_mlg/learn/kelompok/MasukActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/kelompok/MasukActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.learn.kelompok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MasukActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MasukActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_masuk);
+
+        findViewById(R.id.buttonBack).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }

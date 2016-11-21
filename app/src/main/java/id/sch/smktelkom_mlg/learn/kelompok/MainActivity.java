2a56de345b33f381230a9ed33c437d9270c15796
ASUS-A455l <nuraniiin38@gmail.com>
2016-11-21 12:58:39 +0700

    5

--- app/src/main/java/id/sch/smktelkom_mlg/learn/kelompok/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/kelompok/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.buttonplay).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.imageViewplay).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, MasukActivity.class));
